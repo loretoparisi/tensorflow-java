d68509a66ab0695a7e893e77b8f983460a0a5e50
Loreto Parisi <loretoparisi@gmail.com>
2017-03-10 18:37:34 +0100

    Initial import

---
+++ TensorFlowExample.java
@@ -0,0 +1,7 @@
+import org.tensorflow.TensorFlow;
+
+public class TensorFlowExample {
+  public static void main(String[] args) {
+    System.out.println("TensorFlowExample using TensorFlow version: " +  TensorFlow.version());
+  }
+}
