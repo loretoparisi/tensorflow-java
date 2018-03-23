70438e8743d88d8d975ab8f9f907e0e16858c2a3
Loreto Parisi <loretoparisi@gmail.com>
2018-03-23 10:21:51 +0100

    Updated to TF 1.6.0

--- jni.sh
+++ jni.sh
@@ -4,8 +4,8 @@
  OS=$(uname -s | tr '[:upper:]' '[:lower:]')
  mkdir -p ./jni
  curl -L \
-   "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_jni-${TF_TYPE}-${OS}-x86_64-1.4.0.tar.gz" |
+   "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_jni-${TF_TYPE}-${OS}-x86_64-1.6.0.tar.gz" |
    tar -xz -C ./jni
 
   curl -L \
-    -o lib/libtensorflow-1.4.0.jar "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-1.4.0.jar"
+    -o lib/libtensorflow-1.4.0.jar "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-1.6.0.jar"
